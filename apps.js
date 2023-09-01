315706ced9e8bea2e1efc974229a24bd2ae1dc14
Muhammad-Farasat <139209158+Muhammad-Farasat@users.noreply.github.com>
2023-09-01 19:33:29 +0500

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,38 @@
+// alert('How are you');
+
+// alert('Error! Please enter a valid password');
+
+// alert('Welcome to JS land...\nHappy Coding! ');
+
+// alert('Welcome to JS land');
+
+// confirm('Happy Coding!\nPreserve this page from creating additional dialogs');
+
+// let farasat = alert('Hello..I can run through JS web browers console.');
+// console.log(farasat);
+
+// var username;
+// var username = 'Muhammad Farasat';
+
+// let message = alert('hello world');
+
+// var firstName = 'Farasat';
+// var age = '18';
+// var address = '128 big mansion';
+// var course = 'Web development';
+
+// var biodata = 'NAME:'+ firstName + '\n'+ 'AGE:'+ age +'\n'+ 'ADDRESS:' + address +'\n' + 'COURSE:' + course;
+
+// alert(biodata);
+
+// var email = 'farasatkhan687@gmail.com';
+// alert('My email address is '+ email);
+
+// var book = 'A smarter way to learn Javascript';
+// alert('I am trying learn from book' + book);
+
+// var myName = 'Yah! I can write HTML content through Javascript';
+// document.write("<h1>"+myName+"</h1>");
+
+var blah = '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬';
+alert(blah);
